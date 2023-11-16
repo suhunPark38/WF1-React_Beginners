0195d6c4241ca7e61d266e1d446f1fb52d645d98
suhunPark38 <121748597+suhunPark38@users.noreply.github.com>
2023-11-16 22:25:49 +0900

    추가한 코드에 대한 주석 추가

--- react-project/src/components/EstimateTable.js
+++ react-project/src/components/EstimateTable.js
@@ -8,6 +8,10 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 
+/*
+견적 페이지에 나타낼 태이블. 여러 종류의 부품을 기입하다보니 이름만으로 텍스트가 너무 길어져서 깔끔하게 하고자 테이블로 표시함
+할 수 있다면 이미지 or 최종 가격 정도는 표시하면 좋을듯
+ */
 const EstimateTable = ()=> {
     return(
         <TableContainer component={Paper}>
--- react-project/src/data/Estimate.js
+++ react-project/src/data/Estimate.js
@@ -1,3 +1,7 @@
+/*
+견적 데이터가 저장될 공간.
+원래는 products에서 배열을 import해서 작성하려고 했으나 간단하지 않아 일단은 일일히 작성함.
+ */
 const estimates = [
     {type:'최고사양 견적', cpu: '인텔 코어i7-14세대 14700K', gpu:'XFX 라데온 RX 6700 SWFT 309 CORE D6 10GB', board:'MSI MAG B760M 박격포 맥스 WIFI', ram:'마이크론 Crucial DDR5-5600 CL46 PRO 패키지 대원씨티에스 (32GB(16Gx2))', ssd:'SK하이닉스 Gold P31 M.2 NVMe (500GB)', power:'잘만 MegaMax 600W 80PLUS STANDARD', computerCase:'ABKO SUITMASTER AL1000 그레이 (빅타워)', fan: 'ASUS ROG RYUJIN III 360 ARGB WHITE EDITION'},
     {type:'비싼 견적', cpu:'인텔 코어i7-14세대 14700K', gpu:'ASUS GeForce RTX 4090 TUF GAMING O24G OC D6X 24GB', board:'ROG MAXIMUS Z690 EXTREME GLACIAL STCOM (인텔Z690/E-ATX)', ram:'CORSAIR DDR5-5600 CL40 VENGEANCE RGB BLACK', ssd:'CORSAIR MP600 PRO LPX M.2 NVMe 2280 8TB', power:'ASUS ROG STRIX THOR 1600T (ATX/1600W)', computerCase:'ABKO SUITMASTER AL1000 그레이 (빅타워)', fan:'ASUS ROG RYUJIN III 360 ARGB WHITE EDITION'},
