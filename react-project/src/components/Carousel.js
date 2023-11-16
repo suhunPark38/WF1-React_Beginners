aa2b224900fe03aaa11ace257c56bf8d6fb7b054
taemin1971355 <19taemin@hansung.ac.kr>
2023-11-16 15:28:26 +0900

    개망나니 코드 버전

--- react-project/src/components/Carousel.js
+++ react-project/src/components/Carousel.js
@@ -1,7 +1,8 @@
-import React, { useRef, useState } from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
+import products from "../data/products";
 import {
   Paper,
   Button,
@@ -19,28 +20,36 @@ const Carousel = () => {
   const [activeStep, setActiveStep] = useState(0);
   const [selectedRadioGroup1, setSelectedRadioGroup1] = useState('');
   const [selectedRadioGroup2, setSelectedRadioGroup2] = useState('');
+  const [selectedRadioGroup3, setSelectedRadioGroup3] = useState('');
+  const [selectedRadioGroup4, setSelectedRadioGroup4] = useState('');
+  const [selectedRadioGroup5, setSelectedRadioGroup5] = useState('');
+  const [selectedRadioGroup6, setSelectedRadioGroup6] = useState('');
+  const [selectedRadioGroup7, setSelectedRadioGroup7] = useState('');
+  const [selectedRadioGroup8, setSelectedRadioGroup8] = useState('');
   const [budgetSliderValue, setBudgetSliderValue] = useState(100);
   const [cpuOptions, setCpuOptions] = useState([]);
+  const [motherboardOptions, setMotherboardOptions] = useState([]);
+  const [ramOptions, setRamOptions] = useState([]);
+  const [ssdOptions, setSSDOptions] = useState([]);
+  const [powerOptions, setPowerOptions] = useState([]);
+  const [caseOptions, setCaseOptions] = useState([]);
+  const [coolerOptions, setCoolerOptions] = useState([]);
 
   const items = [
-    {
-      name: '컴퓨터 견적을 위해 도와드립니다.',
-    },
+    { name: '컴퓨터 견적을 위해 도와드립니다.' },
     {
       name: '컴퓨터를 사용하는 목적이 무엇인가요?',
       radioGroup: 'group1',
       options: ['사무용', '일상용', '게임용', '고사양', '일단 넘어갈게요.'],
     },
-    {
-      name: '예산을 한정해주세요.',
-      sliderLabel: '예산 선택',
-      sliderValue: budgetSliderValue,
-    },
-    {
-      name: '선호하는 CPU를 골라주세요.',
-      radioGroup: 'group2',
-      options: cpuOptions,
-    },
+    { name: '예산을 한정해주세요.', sliderLabel: '예산 선택', sliderValue: budgetSliderValue },
+    { name: '선호하는 CPU를 골라주세요.', radioGroup: 'group2', options: cpuOptions },
+    { name: '선호하는 메인보드를 골라주세요.', radioGroup: 'group3', options: motherboardOptions },
+    { name: '선호하는 램을 골라주세요.', radioGroup: 'group4', options: ramOptions },
+    { name: '선호하는 SSD를 골라주세요.', radioGroup: 'group5', options: ssdOptions },
+    { name: '선호하는 파워를 골라주세요.', radioGroup: 'group6', options: powerOptions },
+    { name: '선호하는 케이스를 골라주세요.', radioGroup: 'group7', options: caseOptions },
+    { name: '선호하는 쿨러를 골라주세요.', radioGroup: 'group8', options: coolerOptions },
   ];
 
   const settings = {
@@ -71,33 +80,68 @@ const Carousel = () => {
       case 'group2':
         setSelectedRadioGroup2(selectedOption);
         break;
-      // 추가적인 라디오 그룹이 있을 경우에도 처리 가능
+      case 'group3':
+        setSelectedRadioGroup3(selectedOption);
+        break;
+      case 'group4':
+        setSelectedRadioGroup4(selectedOption);
+        break;
+      case 'group5':
+        setSelectedRadioGroup5(selectedOption);
+        break;
+      case 'group6':
+        setSelectedRadioGroup6(selectedOption);
+        break;
+      case 'group7':
+        setSelectedRadioGroup7(selectedOption);
+        break;
+      case 'group8':
+        setSelectedRadioGroup8(selectedOption);
+        break;
       default:
         break;
     }
 
-   switch (selectedOption) {
-         case '사무용':
-           setBudgetSliderValue(55);
-           setCpuOptions(['사무용cpu1', '사무용cpu2', '사무용cpu3']);
-           break;
-         case '일상용':
-           setBudgetSliderValue(105);
-            setCpuOptions(['일상용cpu1', '일상용cpu2', '일상용cpu3']);
-           break;
-        case '게임용':
-           setBudgetSliderValue(195);
-            setCpuOptions(['게임용cpu1', '게임용cpu2', '게임용cpu3']);
-           break;
-        case '고사양':
-           setBudgetSliderValue(625);
-            setCpuOptions(['고사양cpu1', '고사양cpu2', '고사양cpu3']);
-           break;
-         default:
-           setBudgetSliderValue(100);
-           // 기본 값에 대한 처리 추가
-       }
+    switch (selectedOption) {
+      case '사무용':
+        const types = ['CPU', '메인보드', '램', 'SSD', '파워', '케이스', '쿨러'];
 
+        const setOptionsByType = (setType, stateSetter) => {
+          const options = types.reduce((acc, type) => {
+            const filteredProducts = products
+              .filter((product) => product.type === type)
+              .map((product) => product.name);
+            acc[type] = filteredProducts;
+            return acc;
+          }, {});
+
+          stateSetter(options[setType]);
+        };
+
+        setBudgetSliderValue(55);
+        setOptionsByType('CPU', setCpuOptions);
+        setOptionsByType('메인보드', setMotherboardOptions);
+        setOptionsByType('램', setRamOptions);
+        setOptionsByType('SSD', setSSDOptions);
+        setOptionsByType('파워', setPowerOptions);
+        setOptionsByType('케이스', setCaseOptions);
+        setOptionsByType('쿨러', setCoolerOptions);
+
+        break;
+      case '일상용':
+        setBudgetSliderValue(105);
+        setCpuOptions(['일상용cpu1', '일상용cpu2', '일상용cpu3']);
+        break;
+      case '게임용':
+        setBudgetSliderValue(195);
+        setCpuOptions(['게임용cpu1', '게임용cpu2', '게임용cpu3']);
+        break;
+      case '고사양':
+        setBudgetSliderValue(625);
+        setCpuOptions(['고사양cpu1', '고사양cpu2', '고사양cpu3']);
+        break;
+      default:
+    }
   };
 
   // 다음 버튼이 활성화되어야 하는지 여부를 결정하는 함수
@@ -105,6 +149,18 @@ const Carousel = () => {
     if (activeStep === 1 && selectedRadioGroup1 === '') {
       return true;
     } else if (activeStep === 3 && selectedRadioGroup2 === '') {
+      return true;
+    } else if (activeStep === 4 && selectedRadioGroup3 === '') {
+      return true;
+    } else if (activeStep === 5 && selectedRadioGroup4 === '') {
+      return true;
+    } else if (activeStep === 6 && selectedRadioGroup5 === '') {
+      return true;
+    } else if (activeStep === 7 && selectedRadioGroup6 === '') {
+      return true;
+    } else if (activeStep === 8 && selectedRadioGroup7 === '') {
+      return true;
+    } else if (activeStep === 9 && selectedRadioGroup8 === '') {
       return true;
     }
     return false;
@@ -134,8 +190,32 @@ const Carousel = () => {
                   <Typography variant="h6">{item.sliderLabel}</Typography>
                   <input
                     type="range"
-                    min={selectedRadioGroup1 === '일단 넘어갈게요.' ? 30 : selectedRadioGroup1 === '사무용' ? 30 : selectedRadioGroup1 === '일상용' ? 80 : selectedRadioGroup1 === '게임용' ? 130 : selectedRadioGroup1 === '고사양' ? 250: undefined}
-                    max={selectedRadioGroup1 === '일단 넘어갈게요.' ? 1000 : selectedRadioGroup1 === '사무용' ? 80 : selectedRadioGroup1 === '일상용' ? 130 : selectedRadioGroup1 === '게임용' ? 250 : selectedRadioGroup1 === '고사양' ? 1000: undefined}
+                    min={
+                      selectedRadioGroup1 === '일단 넘어갈게요.'
+                        ? 30
+                        : selectedRadioGroup1 === '사무용'
+                        ? 30
+                        : selectedRadioGroup1 === '일상용'
+                        ? 80
+                        : selectedRadioGroup1 === '게임용'
+                        ? 130
+                        : selectedRadioGroup1 === '고사양'
+                        ? 250
+                        : undefined
+                    }
+                    max={
+                      selectedRadioGroup1 === '일단 넘어갈게요.'
+                        ? 1000
+                        : selectedRadioGroup1 === '사무용'
+                        ? 80
+                        : selectedRadioGroup1 === '일상용'
+                        ? 130
+                        : selectedRadioGroup1 === '게임용'
+                        ? 250
+                        : selectedRadioGroup1 === '고사양'
+                        ? 1000
+                        : undefined
+                    }
                     step={5}
                     value={budgetSliderValue}
                     onChange={(e) => setBudgetSliderValue(parseInt(e.target.value))}
@@ -168,6 +248,111 @@ const Carousel = () => {
                   name="radioGroup2"
                   value={selectedRadioGroup2}
                   onChange={(e) => handleRadioChange(e, 'group2')}
+                >
+                  {item.options.map((option, optionIndex) => (
+                    <FormControlLabel
+                      key={optionIndex}
+                      value={option}
+                      control={<Radio />}
+                      label={option}
+                    />
+                  ))}
+                </RadioGroup>
+              )}
+
+              {/* 5번째 슬라이드에서 라디오 그룹 */}
+              {index === 4 && (
+                <RadioGroup
+                  name="radioGroup3"
+                  value={selectedRadioGroup3}
+                  onChange={(e) => handleRadioChange(e, 'group3')}
+                >
+                  {item.options.map((option, optionIndex) => (
+                    <FormControlLabel
+                      key={optionIndex}
+                      value={option}
+                      control={<Radio />}
+                      label={option}
+                    />
+                  ))}
+                </RadioGroup>
+              )}
+
+              {/* 6번째 슬라이드에서 라디오 그룹 */}
+              {index === 5 && (
+                <RadioGroup
+                  name="radioGroup4"
+                  value={selectedRadioGroup4}
+                  onChange={(e) => handleRadioChange(e, 'group4')}
+                >
+                  {item.options.map((option, optionIndex) => (
+                    <FormControlLabel
+                      key={optionIndex}
+                      value={option}
+                      control={<Radio />}
+                      label={option}
+                    />
+                  ))}
+                </RadioGroup>
+              )}
+
+              {/* 7번째 슬라이드에서 라디오 그룹 */}
+              {index === 6 && (
+                <RadioGroup
+                  name="radioGroup5"
+                  value={selectedRadioGroup5}
+                  onChange={(e) => handleRadioChange(e, 'group5')}
+                >
+                  {item.options.map((option, optionIndex) => (
+                    <FormControlLabel
+                      key={optionIndex}
+                      value={option}
+                      control={<Radio />}
+                      label={option}
+                    />
+                  ))}
+                </RadioGroup>
+              )}
+
+              {index === 7 && (
+                <RadioGroup
+                  name="radioGroup6"
+                  value={selectedRadioGroup6}
+                  onChange={(e) => handleRadioChange(e, 'group6')}
+                >
+                  {item.options.map((option, optionIndex) => (
+                    <FormControlLabel
+                      key={optionIndex}
+                      value={option}
+                      control={<Radio />}
+                      label={option}
+                    />
+                  ))}
+                </RadioGroup>
+              )}
+
+              {index === 8 && (
+                <RadioGroup
+                  name="radioGroup7"
+                  value={selectedRadioGroup7}
+                  onChange={(e) => handleRadioChange(e, 'group7')}
+                >
+                  {item.options.map((option, optionIndex) => (
+                    <FormControlLabel
+                      key={optionIndex}
+                      value={option}
+                      control={<Radio />}
+                      label={option}
+                    />
+                  ))}
+                </RadioGroup>
+              )}
+
+              {index === 9 && (
+                <RadioGroup
+                  name="radioGroup8"
+                  value={selectedRadioGroup8}
+                  onChange={(e) => handleRadioChange(e, 'group8')}
                 >
                   {item.options.map((option, optionIndex) => (
                     <FormControlLabel
