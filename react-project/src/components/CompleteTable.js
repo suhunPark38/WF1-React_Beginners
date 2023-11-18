b81a8b062f02bebe4afa4f523f9f46294fcd83b6
taemin1971355 <19taemin@hansung.ac.kr>
2023-11-18 15:54:36 +0900

    CompleteTable.js 총가격 추가 버전

--- react-project/src/components/CompleteTable.js
+++ react-project/src/components/CompleteTable.js
@@ -3,6 +3,10 @@ import React from 'react';
 import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper } from '@mui/material';
 
 const CompleteTable = ({ data }) => {
+  const calculateTotalPrice = () => {
+    return data.reduce((total, product) => total + product.price, 0);
+  };
+
   return (
     <TableContainer component={Paper}>
       <Table>
@@ -12,19 +16,21 @@ const CompleteTable = ({ data }) => {
             <TableCell>이미지</TableCell>
             <TableCell>제품명</TableCell>
             <TableCell>가격</TableCell>
-
           </TableRow>
         </TableHead>
         <TableBody>
           {data.map((product) => (
             <TableRow key={product.id}>
               <TableCell>{product.type}</TableCell>
-              <TableCell><img className="pro-icon" src={process.env.PUBLIC_URL+product.image} alt={product.name} /></TableCell>
+              <TableCell><img className="pro-icon" src={process.env.PUBLIC_URL + product.image} alt={product.name} /></TableCell>
               <TableCell>{product.name}</TableCell>
               <TableCell>{product.price}</TableCell>
-
             </TableRow>
           ))}
+          <TableRow>
+            <TableCell colSpan={3}>총 가격</TableCell>
+            <TableCell>{calculateTotalPrice()}</TableCell>
+          </TableRow>
         </TableBody>
       </Table>
     </TableContainer>
