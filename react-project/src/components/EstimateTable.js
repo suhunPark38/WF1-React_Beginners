4c344cbce2d210f9e60e7bfea406d66d7569fdb0
suhunPark38 <121748597+suhunPark38@users.noreply.github.com>
2023-11-21 22:20:03 +0900

    견적 페이지에 견적 다운로드 기능 추가

--- react-project/src/components/EstimateTable.js
+++ react-project/src/components/EstimateTable.js
@@ -8,8 +8,10 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import ProductModal from "./ProductModal";
-import EstimateModal from "./EstimateModal";
 import products from "../data/products"
+import Button from '@mui/material/Button';
+import DownloadIcon from '@mui/icons-material/Download';
+import {saveAs} from 'file-saver';
 
 /*
 견적 페이지에 나타낼 태이블. 여러 종류의 부품을 기입하다보니 이름만으로 텍스트가 너무 길어져서 깔끔하게 하고자 테이블로 표시함
@@ -35,6 +37,11 @@ const getTotalPerformance = (row) => (
         + products.find((product)=>(product.name === row.computerCase)).performance
 )
 
+const arrayToFile= data=> {
+    const file = new Blob([JSON.stringify(data)],{type: 'text/plain;charset=utf-8'});
+    saveAs(file, "컴퓨터 견본 견적.txt");
+}
+
 const EstimateTable = ()=> (
         <TableContainer component={Paper}>
             <Table>
@@ -59,7 +66,9 @@ const EstimateTable = ()=> (
                         >
                             <TableCell component="th" scope="row">
                                 {row.type}
-                                {EstimateModal(row.type, getTotalPrice(row), getTotalPerformance(row))}
+                                <p>{"총 가격: "+getTotalPrice(row)}</p>
+                                <p>{"총 성능: "+getTotalPerformance(row)}</p>
+                                <Button onClick={()=>arrayToFile(row)}> <DownloadIcon/></Button>
                             </TableCell>
                             <TableCell align="left">{row.cpu}{ProductModal(row.cpu)}</TableCell>
                             <TableCell align="left">{row.gpu}{ProductModal(row.gpu)}</TableCell>
